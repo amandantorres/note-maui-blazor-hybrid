59dac8f005d1f2c308602751aa478363777f5392
amandantorres <amandantorres@gmail.com>
2023-10-04 17:25:40 -0300

    bug fix

---
+++ NotesMauiBlazorWasm.Common/States/GlobalAppState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesMauiBlazorWasm.Common.States
+{
+    public class GlobalAppState : INotifyPropertyChanged
+    {
+        private bool _isInitializing = true;
+
+        public bool IsInitializing
+        {
+            get => _isInitializing;
+            set
+            {
+                _isInitializing = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsInitializing)));
+            }
+        }
+
+        private string? _errorMessage;
+        public string? ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { 
+                _errorMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsInitializing)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
